dad58abac54791cb68220d825686ab1e67be80b2
Julien <jbrochier@ensc.fr>
2018-10-26 12:29:00 +0200

    taf

---
+++ Partie 1/Partie 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Partie_1
+{
+    public partial class Partie_2 : Form
+    {
+        public Partie_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
