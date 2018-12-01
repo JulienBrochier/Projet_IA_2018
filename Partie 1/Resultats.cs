fc8b7ea8b859c803f74ddde047f81c6c546c7266
Julien <jbrochier@ensc.fr>
2018-12-01 23:06:27 +0100

    fin

--- Partie 1/Resultats.cs
+++ Partie 1/Resultats.cs
@@ -14,9 +14,13 @@ namespace Partie_1
     public partial class Resultats : Form
     {
         public double points;
+        private bool dijkstra;
+        Form1 algo;
         public Resultats()
         {
             InitializeComponent();
+            dijkstra = false;
+            algo = new Form1();
         }
 
         private void Titre_Click(object sender, EventArgs e)
@@ -36,10 +40,22 @@ namespace Partie_1
 
         internal void BtnPartie2_Click(object sender, EventArgs e)
         {
-            Form1 algo = new Form1();
-            algo.Show();
+            if (!dijkstra)
+            {
+                algo.Show();
 
-            Total.Text = "" + (points+algo.reussite) + "/20";
+                Total.Text = "" + (points) + "/17";
+
+                label1.Text = "Vous venez de résoudre l'algorithme de Dijsktra, pour actualiser votre score cliquez-ici";
+                dijkstra = true;
+            }
+
+            else
+            {
+                Total.Text = "" + (points+algo.reussite) + "/20";
+                label1.Text = "Merci d'avoir complété ce test !!";
+            }
+
         }
 
         private void label1_Click(object sender, EventArgs e)
